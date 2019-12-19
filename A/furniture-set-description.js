b84f8b21afe5ca229136b99100462ada49c5d4ba
tarnsmt <tarnsmt1139@gmail.com>
2019-12-19 21:07:49 +0700

    Furniture set description ui edit yayyay

--- A/furniture-set-description.js
+++ A/furniture-set-description.js
@@ -1,94 +1,261 @@
 import Backend from "./backend.js";
 
-var backend = new Backend()
+var app = window.app || {},
+    business_paypal = '';
 
-var urlParam = new URLSearchParams(location.search);
-var id = urlParam.get("id")
+var backend = new Backend();
 
-var Chef = {
-    init: function () {
-        this.preserveProduct().then(
-            () => {
-                this.productImagePreview();
-                this.menuToggle();
-                this.misc();
-            }
-        );
-    },
+(function ($) {
+    'use strict';
 
-    preserveProduct: async function () {
-        try {
-            let response = await backend.getProduct(["productCode"], [id]);
-            let item = response[0];
-            let htmlList = "";
-            for (let index = 0; index < item.images.length; index++) {
-                if (index === 0) {
-                    htmlList += '<li class="preview"><img src="' + item.images[index] + '" alt=""></li>';
-                    htmlList += '<li><a href="javascript:void(0)"><img src="' + item.images[index] + '" alt=""></a></li>';
-                    continue;
-                }
-                htmlList += '<li><a href="javascript:void(0)"><img src="' + item.images[index] + '" alt=""></a></li>'
-            }
 
-            document.getElementById("imageList").innerHTML = htmlList;
-            document.getElementById("product-name").innerText = item.name;
-            document.getElementById("product-price").innerHTML = '<span>$    </span><span>' + item.price + '</span>'
-            document.getElementById("product-description").innerHTML = '<p>' + item.description + '</p>'
-        } catch (e) {
-            if (e.name === 'TypeError') {
-                document.getElementById("imageList").innerHTML = '<li class="preview"><img src="images/product-description/download.jpg" alt=""></li>' +
-                    '<li><a href="javascript:void(0)"><img src="images/product-description/download.jpg" alt=""></a></li>' +
-                    '<li><a href="javascript:void(0)"><img src="images/product-description/download-1.jpg" alt=""></a></li>' +
-                    '<li><a href="javascript:void(0)"><img src="images/product-description/download-2.jpg" alt=""></a></li>' +
-                    '<li><a href="javascript:void(0)"><img src="images/product-description/download-3.jpg" alt=""></a></li>' +
-                    '<li><a href="javascript:void(0)"><img src="images/product-description/download-4.jpg" alt=""></a></li>'
-                document.getElementById("product-name").innerText = 'Modern Table';
-                document.getElementById("product-price").innerHTML = '<span>$ 1000</span>'
-                document.getElementById("product-description").innerHTML = '<p>This string is randomly generated. The standard default text is designed to ramble about nothing. The standard default text is designed to ramble about nothing. This string is randomly  generated.</p>' +
-                    '<p>Whoever evaluates your text cannot evaluate the way you write. Default text creates the illusion of real text. Your design looks awesome by the way. If it is not real text, they will focus on the design.</p>'
-            } else console.log(e);
-        }
+    app.init = function () {
 
-    },
+        var total = 0,
+            items = 0
 
-    productImagePreview: function () {
-        $(document).on('click', '.product-images li', function () {
-            if (!$(this).hasClass('preview')) {
-                var src = $(this).find('img').attr('src');
-                $('.product-images .preview img').attr('src', src);
-            }
-        });
-    },
+        var cart = (JSON.parse(localStorage.getItem('cart')) != null) ? JSON.parse(localStorage.getItem('cart')) : {items: []};
 
-    menuToggle: function () {
-        $(document).on('click', '#menu .trigger', function () {
-            // Toggle open and close icons
-            $(this).find('img').each(function () {
-                if ($(this).hasClass('hidden')) {
-                    $(this).removeClass('hidden');
-                } else {
-                    $(this).addClass('hidden');
-                }
+        if (undefined != cart.items && cart.items != null && cart.items != '' && cart.items.length > 0) {
+            _.forEach(cart.items, function (n, key) {
+                items = (items + n.cant)
+                total = total + (n.cant * n.price)
             });
 
-            // Toggle menu links
-            $(this).siblings('.links').stop(true, true).slideToggle(200);
+        }
 
-            // Toggle open class
-            $('#menu').toggleClass('open');
-        });
-    },
+        $('#totalItems').text(items)
+        $('.totalAmount').text('$ ' + total + ' USD')
 
-    misc: function () {
-        // Misc stuff
+    }
 
-        for (var i = 1; i <= 3; i++) {
-            $('.product').parent().eq(0).clone().appendTo('.product-list');
+    app.createProducts = async function () {
+        var additionalProduct = await backend.getProduct(["furnitureType"], ["Bed"]);
+        var productos = [
+                {
+                    productCode: 1,
+                    images: ['images/productlisting/table1.jpg'],
+                    name: 'Table1',
+                    price: 299.00,
+                    desc: 'Libertad 5oz BU 1998 Contains 1 Libertad 5oz BU brilliant uncirculated .999 fine Silver. In capsule The same coin as you see in this picture. We only Ship to the US, and is FREE Shipping Shipping time 5-7 business days via UPS express with tracking and insurance. Payments only via Paypal.',
+                    stock: 4
+                },
+                {
+                    productCode: 2,
+                    name: 'Table2',
+                    images: ['images/productlisting/table2.jpg'],
+                    price: 199.00,
+                    desc: 'Libertad 5oz BU 1998 Contains 1 Libertad 5oz BU brilliant uncirculated .999 fine Silver. In capsule The same coin as you see in this picture. We only Ship to the US, and is FREE Shipping Shipping time 5-7 business days via UPS express with tracking and insurance. Payments only via Paypal.',
+                    stock: 2
+                },
+                {
+                    productCode: 3,
+                    name: 'Table3',
+                    images: ['images/productlisting/table3.jpg'],
+                    price: 99.00,
+                    desc: 'Libertad 5oz BU 1998 Contains 1 Libertad 5oz BU brilliant uncirculated .999 fine Silver. In capsule The same coin as you see in this picture. We only Ship to the US, and is FREE Shipping Shipping time 5-7 business days via UPS express with tracking and insurance. Payments only via Paypal.',
+                    stock: 1
+                },
+                // {
+                //     id: 4,
+                //     name: 'Table4',
+                //     image: ['images/pic01.jpg'],
+                //     price: 80.00,
+                //     desc: 'Libertad 5oz BU 1998 Contains 1 Libertad 5oz BU brilliant uncirculated .999 fine Silver. In capsule The same coin as you see in this picture. We only Ship to the US, and is FREE Shipping Shipping time 5-7 business days via UPS express with tracking and insurance. Payments only via Paypal.',
+                //     stock: 0
+                // },
+                {
+                    productCode: 1,
+                    images: ['images/productlisting/table4.jpg'],
+                    name: 'Table5',
+                    price: 10.00,
+                    desc: 'Libertad 5oz BU 1998 Contains 1 Libertad 5oz BU brilliant uncirculated .999 fine Silver. In capsule The same coin as you see in this picture. We only Ship to the US, and is FREE Shipping Shipping time 5-7 business days via UPS express with tracking and insurance. Payments only via Paypal.',
+                    stock: 8
+                }
+            ],
+            wrapper = $('.productosWrapper'),
+            contenido = ''
+
+        productos = productos.concat(additionalProduct);
+        for (var i = 0; i < productos.length; i++) {
+            let ref = 'product-description.html?id=' + productos[i].productCode;
+            if (1) {
+                contenido += '<div class="coin-wrapper"><a href=' + ref + '>';
+                contenido += '		<img src="' + productos[i].images[0] + '" alt="' + productos[i].name + '">  <div class="middle"><a href=' + ref + '></a></div>'
+                contenido += '		<span class="large-12 columns product-details">'
+                contenido += '			<h3>' + productos[i].name + ' <span class="price">$ ' + productos[i].price + ' USD</span></h3>'
+                // contenido += '			<h3>Stock: <span class="stock">' + productos[i].stock + '</span></h3>'
+                contenido += '		</span>'
+                // contenido += '		<a class="large-12 columns btn submit ladda-button prod-' + productos[i].id + '" data-style="slide-right" onclick="app.addtoCart(' + productos[i].id + ');">More Info</a>'
+                contenido += '		<div class="clearfix"></div>'
+                contenido += '</div>'
+
+            }
+
+        }
+
+        wrapper.html(contenido)
+
+        localStorage.setItem('productos', JSON.stringify(productos))
+    };
+
+    app.addtoCart = function (id) {
+        var l = Ladda.create(document.querySelector('.prod-' + id));
+
+        l.start();
+        var productos = JSON.parse(localStorage.getItem('productos')),
+            producto = _.find(productos, {'id': id}),
+            cant = 1
+        if (cant <= producto.stock) {
+            if (undefined != producto) {
+                if (cant > 0) {
+                    setTimeout(function () {
+                        var cart = (JSON.parse(localStorage.getItem('cart')) != null) ? JSON.parse(localStorage.getItem('cart')) : {items: []};
+                        app.searchProd(cart, producto.id, parseInt(cant), producto.name, producto.price, producto.img, producto.stock)
+                        l.stop();
+                    }, 2000)
+                } else {
+                    alert('Solo se permiten cantidades mayores a cero')
+                }
+            } else {
+                alert('Oops! algo malo ocurrió, inténtalo de nuevo más tarde')
+            }
+        } else {
+            alert('No stock available')
         }
     }
-};
 
-$(function () {
-    Chef.init();
-});
+    app.searchProd = function (cart, id, cant, name, price, img, available) {
+
+        var curProd = _.find(cart.items, {'id': id})
+
+        if (undefined != curProd && curProd != null) {
+
+            if (curProd.cant < available) {
+                curProd.cant = parseInt(curProd.cant + cant)
+            } else {
+                alert('No stock available')
+            }
+
+        } else {
+
+            var prod = {
+                id: id,
+                cant: cant,
+                name: name,
+                price: price,
+                img: img,
+                available: available
+            }
+            cart.items.push(prod)
+
+        }
+        localStorage.setItem('cart', JSON.stringify(cart))
+        app.init()
+        app.getProducts()
+        app.updatePayForm()
+    }
+
+    app.getProducts = function () {
+        var cart = (JSON.parse(localStorage.getItem('cart')) != null) ? JSON.parse(localStorage.getItem('cart')) : {items: []},
+            msg = '',
+            wrapper = $('.cart'),
+            total = 0
+        wrapper.html('')
+
+        if (undefined == cart || null == cart || cart == '' || cart.items.length == 0) {
+            wrapper.html('<li>Cart is empty &nbsp; &nbsp;</li>');
+            $('.cart').css('left', '-400%')
+        } else {
+            var items = '';
+            _.forEach(cart.items, function (n, key) {
+
+                total = total + (n.cant * n.price)
+                items += '<li>'
+                items += '<img src="' + n.img + '" />'
+                items += '<h3 class="title">' + n.name + '<br><span class="price">' + n.cant + ' x $ ' + n.price + ' USD</span> </h3>'
+                items += '</li>'
+            });
+
+
+            items += '<li id="total">Total : $ ' + total + ' <div id="submitForm"></div></li>'
+            wrapper.html(items)
+            $('.cart').css('left', '-500%')
+        }
+    }
+
+    app.updateItem = function (id, available) {
+
+        var cart = (JSON.parse(localStorage.getItem('cart')) != null) ? JSON.parse(localStorage.getItem('cart')) : {items: []},
+            curProd = _.find(cart.items, {'id': id})
+
+        curProd.cant = curProd.cant - 1;
+
+        if (curProd.cant > 0) {
+            localStorage.setItem('cart', JSON.stringify(cart))
+            app.init()
+            app.getProducts()
+            app.updatePayForm()
+        } else {
+            app.deleteProd(id, true)
+        }
+    }
+
+    app.delete = function (id) {
+        var cart = (JSON.parse(localStorage.getItem('cart')) != null) ? JSON.parse(localStorage.getItem('cart')) : {items: []};
+        var curProd = _.find(cart.items, {'id': id})
+        _.remove(cart.items, curProd);
+        localStorage.setItem('cart', JSON.stringify(cart))
+        app.init()
+        app.getProducts()
+        app.updatePayForm()
+    }
+
+    app.deleteProd = function (id, remove) {
+        if (undefined != id && id > 0) {
+
+            if (remove == true) {
+                app.delete(id)
+            } else {
+                var conf = confirm('Are you sure you want to delete?')
+                if (conf) {
+                    app.delete(id)
+                }
+            }
+
+        }
+    }
+
+    app.updatePayForm = function () {
+        var cart = (JSON.parse(localStorage.getItem('cart')) != null) ? JSON.parse(localStorage.getItem('cart')) : {items: []};
+        var statics = '<form action="https://www.paypal.com/cgi-bin/webscr" method="post"><input type="hidden" name="cmd" value="_cart"><input type="hidden" name="upload" value="1"><input type="hidden" name="currency_code" value="USD" /><input type="hidden" name="business" value="' + business_paypal + '">',
+            dinamic = '',
+            wrapper = $('#submitForm')
+
+        wrapper.html('')
+
+        if (undefined != cart && null != cart && cart != '') {
+            var i = 1;
+            _.forEach(cart.items, function (prod, key) {
+                dinamic += '<input type="hidden" name="item_name_' + i + '" value="' + prod.name + '">'
+                dinamic += '<input type="hidden" name="amount_' + i + '" value="' + prod.price + '">'
+                dinamic += '<input type="hidden" name="item_number_' + i + '" value="' + prod.id + '" />'
+                dinamic += '<input type="hidden" name="quantity_' + i + '" value="' + prod.cant + '" />'
+                i++;
+            })
+
+            statics += dinamic + '<button type="submit" class="pay">Check out &nbsp;<i class="ion-chevron-right"></i></button></form>'
+
+            wrapper.html(statics)
+        }
+    }
+
+    $(document).ready(function () {
+        app.init()
+        app.getProducts()
+        app.updatePayForm()
+        app.createProducts()
+    })
+
+})(jQuery)
 
