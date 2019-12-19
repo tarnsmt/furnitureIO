6785860f3c7730ef0b0fb91c0aa82d553a6b96f1
Inter_Nutthapat <nutp1999@gmail.com>
2019-12-19 16:23:57 +0700

    modify for defalut page

--- A/product-description.js
+++ A/product-description.js
@@ -16,23 +16,39 @@ var Chef = {
         );
     },
 
-    preserveProduct: async function() {
-        let response = await backend.getProduct(["productCode"], [id]);
-        let item =  response[0];
-        let htmlList = "";
-        for (let index = 0; index < item.images.length; index++) {
-            if (index === 0) {
-                htmlList += '<li class="preview"><img src="' + item.images[index] + '" alt=""></li>';
-                htmlList += '<li><a href="javascript:void(0)"><img src="' + item.images[index] + '" alt=""></a></li>';
-                continue;
+    preserveProduct: async function () {
+        try {
+            let response = await backend.getProduct(["productCode"], [id]);
+            let item = response[0];
+            let htmlList = "";
+            for (let index = 0; index < item.images.length; index++) {
+                if (index === 0) {
+                    htmlList += '<li class="preview"><img src="' + item.images[index] + '" alt=""></li>';
+                    htmlList += '<li><a href="javascript:void(0)"><img src="' + item.images[index] + '" alt=""></a></li>';
+                    continue;
+                }
+                htmlList += '<li><a href="javascript:void(0)"><img src="' + item.images[index] + '" alt=""></a></li>'
             }
-            htmlList += '<li><a href="javascript:void(0)"><img src="' + item.images[index] + '" alt=""></a></li>'
+
+            document.getElementById("imageList").innerHTML = htmlList;
+            document.getElementById("product-name").innerText = item.name;
+            document.getElementById("product-price").innerHTML = '<span>$    </span><span>' + item.price + '</span>'
+            document.getElementById("product-description").innerHTML = '<p>' + item.description + '</p>'
+        } catch (e) {
+            if (e.name === 'TypeError') {
+                document.getElementById("imageList").innerHTML = '<li class="preview"><img src="images/product-description/download.jpg" alt=""></li>' +
+                    '<li><a href="javascript:void(0)"><img src="images/product-description/download.jpg" alt=""></a></li>' +
+                    '<li><a href="javascript:void(0)"><img src="images/product-description/download-1.jpg" alt=""></a></li>' +
+                    '<li><a href="javascript:void(0)"><img src="images/product-description/download-2.jpg" alt=""></a></li>' +
+                    '<li><a href="javascript:void(0)"><img src="images/product-description/download-3.jpg" alt=""></a></li>' +
+                    '<li><a href="javascript:void(0)"><img src="images/product-description/download-4.jpg" alt=""></a></li>'
+                document.getElementById("product-name").innerText = 'Modern Table';
+                document.getElementById("product-price").innerHTML = '<span>$ 1000</span>'
+                document.getElementById("product-description").innerHTML = '<p>This string is randomly generated. The standard default text is designed to ramble about nothing. The standard default text is designed to ramble about nothing. This string is randomly  generated.</p>' +
+                    '<p>Whoever evaluates your text cannot evaluate the way you write. Default text creates the illusion of real text. Your design looks awesome by the way. If it is not real text, they will focus on the design.</p>'
+            } else console.log(e);
         }
 
-        document.getElementById("imageList").innerHTML = htmlList;
-        document.getElementById("product-name").innerText = item.name;
-        document.getElementById("product-price").innerHTML = '<span>$    </span><span>'+ item.price +'</span>'
-        document.getElementById("product-description").innerHTML = '<p>' + item.description + '</p>'
     },
 
     productImagePreview: function () {
